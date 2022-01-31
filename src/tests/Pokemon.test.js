fd1a3de68b4c079f0c928fb9547b0313c30a343a
Guilherme <guilhermehallmann@gmail.com>
2022-01-31 17:35:37 -0300

    Requisito 6 correto

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import pokemons from '../data';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
-import pokemons from '../data';
-import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 
 const NAME = 'pokemon-name';
 const TYPE = 'pokemon-type';
